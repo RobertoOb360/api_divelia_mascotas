fccf3ac75b706360cb3faadc9d624c54bb3c3bc3
RobertoOb360 <obregonbarturen@gmail.com>
2023-07-25 19:03:03 -0500

    carga final

--- mascotas/models.py
+++ mascotas/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-# Create your models here.
 class Developer(models.Model):
     codigo_empleado=models.IntegerField(primary_key=True)
     nombre=models.CharField(max_length=30)
